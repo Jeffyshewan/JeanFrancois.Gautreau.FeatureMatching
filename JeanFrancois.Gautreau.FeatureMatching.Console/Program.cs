6e538df144184174921388903396625f9667ccbc
Jean-François GAUTREAU <Jeff59550@gmail.com>
2022-02-08 11:30:26 +0100

    Initial commit

---
+++ JeanFrancois.Gautreau.FeatureMatching.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using JeanFrancois.Gautreau.FeatureMatching;
+
+var imageScenesData = new List<byte[]>();
+foreach (var imagePath in
+    Directory.EnumerateFiles(args[1]))
+{
+    var imageBytes = await File.ReadAllBytesAsync(imagePath);
+    imageScenesData.Add(imageBytes);
+}
+var objectImageData = await
+    File.ReadAllBytesAsync(args[0]);
+var detectObjectInScenesResults = new
+    ObjectDetection().DetectObjectInScenes(objectImageData, imageScenesData);
+
+foreach (var objectDetectionResult in detectObjectInScenesResults)
+{
+    System.Console.WriteLine($"Points:{JsonSerializer.Serialize(objectDetectionResult.Points)}");
+}
---
+++ JeanFrancois.Gautreau.FeatureMatching/ObjectDetection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JeanFrancois.Gautreau.FeatureMatching;
+
+public record ObjectDetection()
+{
+    public IList<ObjectDetectionResult> DetectObjectInScenes(byte[] objectImageData, IList<byte[]> imagesSceneData)
+    {
+        var tasks = new List<Task>();
+        var result = new List<ObjectDetectionResult>();
+        foreach (var imageSceneData in imagesSceneData)
+        {
+            var task = Task.Run(() =>
+                result.Add(DetectObjectInScenes(objectImageData, imageSceneData)));
+            tasks.Add(task);
+        }
+
+        Task.WaitAll(tasks.ToArray());
+        return tasks.Select(task => ((Task<ObjectDetectionResult>) task).Result).ToList();
+    }
+}
